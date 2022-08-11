de22b0f885cf8e7112885f33391c17ab10058ec7
Shishir Tambe <93042473+Coldman22@users.noreply.github.com>
2022-08-11 19:04:10 +0530

    Add files via upload

---
+++ threecom.java
@@ -0,0 +1,6 @@
+public class threecom {
+    public static void main(String[] args){
+        /*This code below will print a multiline comment wjich is only read by the user */
+        System.out.println("Hello world!");
+    }
+}
---
+++ twocom.java
@@ -0,0 +1,5 @@
+public class twocom {
+    public static void main(String[] args){
+        System.out.println("Hello world!"); //this is a comment
+    }
+}
