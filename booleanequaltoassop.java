7e8d68ad52d2f2894e1eb6e9efc8dfb48a5d3e6c
Shishir Tambe <93042473+Coldman22@users.noreply.github.com>
2022-08-12 19:33:59 +0530

    Add files via upload

---
+++ booleanequaltoassop.java
@@ -0,0 +1,6 @@
+public class booleanequaltoassop {
+    public static void main(String[] args){
+        int x = 9;
+        System.out.println(x == 9);
+    }
+}
---
+++ trueorfalseexpression.java
@@ -0,0 +1,8 @@
+public class trueorfalseexpression {
+    public static void main(String[] args){
+        int x =9;
+        int y =2;
+        System.out.println(x>y);
+        System.out.println(x<y);
+    }
+}
