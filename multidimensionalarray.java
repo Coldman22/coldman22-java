4cacb278b6a48c15064047d9e80c0b4c2f25b4de
Shishir Tambe <93042473+Coldman22@users.noreply.github.com>
2022-08-13 18:40:37 +0530

    Add files via upload

---
+++ multidimensionalarray.java
@@ -0,0 +1,7 @@
+public class multidimensionalarray {
+    public static void main(String[] args){
+        int [][] myNumbers = {{1,2,3,4},{5,6,7,8,9}};
+        int x = myNumbers[1][4];
+        System.out.println(x);
+    }
+}
