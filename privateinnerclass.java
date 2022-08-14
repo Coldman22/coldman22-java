9ff28108df5a5b74e3f97cff345849bb3aaa3162
Shishir Tambe <93042473+Coldman22@users.noreply.github.com>
2022-08-14 17:28:57 +0530

    Add files via upload

---
+++ privateinnerclass.java
@@ -0,0 +1,17 @@
+class OuterClass{
+    int x = 10;
+
+    private class InnerClass{
+        int y = 5;
+    }
+}
+public class privateinnerclass {
+    public static void main(String[] args){
+        OuterClass myOuter = new OuterClass();
+        OuterClass.InnerClass myInner = myOuter.new InnerClass();
+        System.out.println(myInner.y+myOuter.x);
+    }
+}
+//This will legit show an error
+// using private class InnerClass 
+// and public static void using OuterClass.InnerClass
