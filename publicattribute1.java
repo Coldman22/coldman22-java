537eff8732b8f2a8b81d2ffd553e061bb3e309ea
Shishir Tambe <93042473+Coldman22@users.noreply.github.com>
2022-08-14 17:19:33 +0530

    Add files via upload

---
+++ publicattribute1.java
@@ -0,0 +1,6 @@
+public class publicattribute1{
+    public String fname = "John";
+    public String lname = "Doe";
+    public String email = "johndoe@gmail.com";
+    public int age = 24;
+}
---
+++ publicclasss.java
@@ -0,0 +1,5 @@
+public class publicclasss{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
