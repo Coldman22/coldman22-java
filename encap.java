8218b8be2ae5552643e8d14d69c5c052ee3ee7b3
Shishir Tambe <93042473+Coldman22@users.noreply.github.com>
2022-08-14 17:23:06 +0530

    Add files via upload

---
+++ encap.java
@@ -0,0 +1,7 @@
+public class encap{
+    public static void main(String[] args){
+        person myObj = new person();
+        myObj.setName("John");
+        System.out.println(myObj.getName());
+    }
+}
