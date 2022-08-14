0b62289699605fc2fe76685b79d780e4b85924e6
Shishir Tambe <93042473+Coldman22@users.noreply.github.com>
2022-08-14 17:24:25 +0530

    Add files via upload

---
+++ createpackage.java
@@ -0,0 +1,7 @@
+
+
+class createpackage {
+    public static void main(String[] args){
+        System.out.println("This is my package!");
+    }    
+}
---
+++ importpackagefromapi.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class importpackagefromapi{
+    public static void main(String[] args){
+        Scanner myObj= new Scanner(System.in);
+        String userName;
+
+        //Enter username and press Enter
+        System.out.println("Enter Username ");
+        userName = myObj.nextLine();
+
+        System.out.println("Username is: "+userName);
+    }
+}
