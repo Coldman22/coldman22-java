d08e2d53eb81a62b0c99d4b84b928e89607e414a
Shishir Tambe <93042473+Coldman22@users.noreply.github.com>
2022-08-16 17:03:02 +0530

    Add files via upload

---
+++ throwstat.java
@@ -0,0 +1,12 @@
+public class throwstat {
+    static void checkAge(int age){
+        if(age<18){
+            throw new ArithmeticException("Access denied-You must be at least 18 years old.");
+        }else{
+            System.out.println("Access Granted - You are old enough!");
+        }
+    }
+    public static void main(String[] args){
+        checkAge(15);
+    }
+}
---
+++ trycatchstat.java
@@ -0,0 +1,11 @@
+public class trycatchstat{
+    public static void main(String[] args){
+        try{
+            int[] myNumber={1,2,3};
+
+            System.out.println(myNumber[10]);
+        }catch(Exception e){
+            System.out.println("Something went wrong.");
+        }
+    }
+}
